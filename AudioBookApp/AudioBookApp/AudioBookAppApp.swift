9056853f682ea7b7a52aa85266e359395612d336
Samuel Pinheiro Junior <samuel.pinheirojr@gmail.com>
2025-10-07 17:17:50 -0400

    Initialize SwiftUI AudioBook project

---
+++ AudioBookApp/AudioBookApp/AudioBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBookAppApp.swift
+//  AudioBookApp
+//
+//  Created by Samuel Pinheiro Junior on 2025-10-07.
+//
+
+import SwiftUI
+
+@main
+struct AudioBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioBookApp/AudioBookAppTests/AudioBookAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBookAppTests.swift
+//  AudioBookAppTests
+//
+//  Created by Samuel Pinheiro Junior on 2025-10-07.
+//
+
+import Testing
+@testable import AudioBookApp
+
+struct AudioBookAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
